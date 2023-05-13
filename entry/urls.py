5e8f59beb260af999655cdbe54fa8f36f63c8b03
kennedy-ak <akogokennedy@gmail.com>
2023-05-13 04:19:13 +0000

    performing CRUD Application

--- entry/urls.py
+++ entry/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.List),
-    path("detail/<int:id>",views.detail, name="detail")
+    path("detail/<int:id>",views.detail, name="detail"),
+    path("add",views.addNew,name='add-new'),
+    path("edit/<int:id>",views.edit, name="edit"),
+    path("delete/<int:id>",views.delete, name="delete")
 ]
--- entry/views.py
+++ entry/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Entry# importing the model entries
 
-
+import datetime
 # Create your views here.
 
 
@@ -21,4 +21,42 @@ def detail(request, id):
     entry = Entry.objects.get(id=id)
     context = {'entry': entry}
     return render(request, 'entry/detail.html', context)
+
+
+def addNew(request):
+    current_datetime = datetime.datetime.now()
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        date_created = request.POST['date_created']
+
+        new_entry = Entry.objects.create(title=title,content=content,date_created=date_created)
+        new_entry.save()
+        return redirect('/')
+   
+       
+    
+    return render(request, "entry/form.html",{'current_datetime': current_datetime})
+
+def edit(request,id):
+    current_entry = Entry.objects.get(id = id)
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        # date_created = request.POST['date_created']
+        current_entry.title = title
+        current_entry.content =content
+        # current_entry.date_created =date_created
+        current_entry.save()
+        return redirect('/')
+    
+    return render(request, "entry/edit.html",{"current_entry":current_entry})
+
+def delete(request,id):
+    current = Entry.objects.get(id=id)
+    current.delete()
+    return redirect('/')
+
+
+
 
