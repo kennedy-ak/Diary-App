ecb49bcd826fa0f8983f9b85f86b5758337ca227
kennedy-ak <akogokennedy@gmail.com>
2023-05-13 00:31:35 +0000

    creating a model for the entries

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone # imporing timezone to get the time each entry is made
 
 # Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=300)
+    date_created = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return f"{self.title}"
+    
